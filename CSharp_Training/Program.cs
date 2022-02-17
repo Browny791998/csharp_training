a7960f01502062e5366b989f8fc94ce0d93c5d19
Browny791998 <69458990+Browny791998@users.noreply.github.com>
2022-02-17 13:04:21 +0630

    [feature/tutorial3]finish tutorial 3 100%

--- CSharp_Training/Program.cs
+++ CSharp_Training/Program.cs
@@ -10,7 +10,45 @@ namespace CSharp_Training
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int first, second, result;
+            string opt;
+            Console.Write("Pls write Console Calculator in C# as like below:\n");
+            Console.Write("---------------------------------------------------------");
+            Console.Write("\n\n");
+            Console.WriteLine("Type a number,then press Enter");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            Console.WriteLine("Type another number,then press Enter ");
+            second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("a-Add\ns-Subtract\nm-Multiply\nd-Divide");
+            Console.Write("Your option?");
+            opt = Console.ReadLine();
+            switch (opt)
+            {
+                case "a":
+                    result = first + second;
+                    Console.WriteLine("Your result:{0} + {1} = {2}", first, second, result);
+                    break;
+                case "s":
+                    result = first - second;
+                    Console.WriteLine("Your result:{0} - {1} = {2}", first, second, result);
+                    break;
+                case "m":
+                    result = first * second;
+                    Console.WriteLine("Your result:{0} x {1} = {2}", first, second, result);
+                    break;
+                case "d":
+                    result = first / second;
+                    Console.WriteLine("Your result:{0} ÷ {1} = {2}", first, second, result);
+                    break;
+                default:
+                    Console.WriteLine("Please choose only from opiton");
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
