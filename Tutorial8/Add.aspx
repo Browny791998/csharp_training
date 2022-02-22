cf57e3a103064bbb0ffea5102b6b19e943c87312
Browny791998 <69458990+Browny791998@users.noreply.github.com>
2022-02-22 15:32:43 +0630

    [feature/tutorial8]finish tutorial8 100%

---
+++ Tutorial8/Add.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Add.aspx.cs" Inherits="Tutorial8.Add" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="Content/bootstrap.min.css" />
+</head>
+<body>
+    <form id="form1" runat="server" style="margin-top: 20px">
+        <h1 class="text-center text-info">CRUD Form</h1>
+        <div class="col-md-8 col-md-offset-2">
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-2">
+                        <label>Name</label>
+                    </div>
+                    <div class="col-md-5">
+                        <asp:TextBox ID="txtName" runat="server" CssClass="form-control"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-2 col-md-offset-1">
+                        <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass="btn btn-success" OnClick="btnSubmit_Click" />
+                    </div>
+
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <asp:Label ID="lblMessage" runat="server" CssClass="alert alert-success" Visible="False"></asp:Label>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 col-md-offset-2">
+            <asp:GridView ID="gvPet" runat="server" CssClass="table table-hover" AutoGenerateColumns="False"
+                DataKeyNames="id" OnRowDeleting="gvPet_RowDeleting">
+                <Columns>
+                    <asp:BoundField HeaderText="No" DataField="id" />
+                    <asp:BoundField HeaderText="Cat Name" DataField="name" />
+                    <asp:HyperLinkField Text="Update" DataNavigateUrlFields="id"
+                        DataNavigateUrlFormatString="Edit.aspx?id={0}" HeaderText="Edit" ControlStyle-CssClass="btn btn-info" />
+                    <asp:CommandField ShowDeleteButton="true"
+                        ControlStyle-CssClass="btn btn-danger" HeaderText="Delete" />
+
+                </Columns>
+
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ Tutorial8/Add.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tutorial8
+{
+    public partial class Add : System.Web.UI.Page
+    {
+        string constring = ConfigurationManager.ConnectionStrings["userDB"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetData();
+            }
+        }
+        /// <summary>
+        /// Retrieving all data
+        /// </summary>
+        private void GetData()
+        {
+            SqlConnection con = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand("SELECT * from tbl_Cat", con);
+            con.Open();
+            gvPet.DataSource = cmd.ExecuteReader();
+            gvPet.DataBind();
+            con.Close();
+        }
+
+        /// <summary>
+        /// Adding data
+        /// </summary>
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "")
+            {
+                SqlConnection con = new SqlConnection(constring);
+                SqlCommand cmd = new SqlCommand("INSERT INTO tbl_Cat VALUES(@name)", con);
+                cmd.Parameters.AddWithValue("name", txtName.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                lblMessage.Visible = true;
+                lblMessage.Text = "Added successfully";
+                txtName.Text = "";
+                GetData();
+            }
+            else
+            {
+                lblMessage.Visible = true;
+                lblMessage.CssClass = "alert alert-danger";
+                lblMessage.Text = "Please fill name";
+            }
+        }
+
+        /// <summary>
+        /// Delet data
+        /// </summary>
+        protected void gvPet_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int id = Convert.ToInt32(gvPet.DataKeys[e.RowIndex].Value);
+            SqlConnection con = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand("DELETE FROM tbl_Cat WHERE id=@id", con);
+            cmd.Parameters.AddWithValue("id", id);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            lblMessage.Visible = true;
+            lblMessage.Text = "Deleted successfully";
+            GetData();
+        }
+    }
+}
