c6c09a5daa95a786264c7695520a623c477bdc73
Browny791998 <69458990+Browny791998@users.noreply.github.com>
2022-02-24 17:34:05 +0630

    [feature/tutorial9]finish tutorial 9 100%

---
+++ Tutorial9/Main.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Main.aspx.cs" Inherits="Tutorial9.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnExcel" runat="server" Text="Fetching data from Excel" OnClick="btnExcel_Click" />
+            <asp:Button ID="btnCSV" runat="server" OnClick="btnCSV_Click" Text="Fetching data from CSV file" />
+            <asp:Button ID="btnText" runat="server" OnClick="btnText_Click" Text="Fetching data from Text file" />
+            <br />
+            <br />
+            <asp:GridView ID="gvFile" runat="server" CellPadding="3" GridLines="Horizontal" AutoGenerateColumns="False" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px">
+                <AlternatingRowStyle BackColor="#F7F7F7"/>
+                <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+                <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+                <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+                <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <SortedAscendingCellStyle BackColor="#F4F4FD" />
+                <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+                <SortedDescendingCellStyle BackColor="#D8D8F0" />
+                <SortedDescendingHeaderStyle BackColor="#3E3277" />
+
+
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" />
+                     <asp:BoundField DataField="Name" HeaderText="Name" />
+                     <asp:BoundField DataField="Address" HeaderText="Address" />
+                       <asp:BoundField DataField="Email" HeaderText="Email" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ Tutorial9/Main.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+using System.Data;
+
+namespace Tutorial9
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string excelfilepath = Server.MapPath("~/Upload/User.xlsx");
+            ImoprtExceltoGridView(excelfilepath, ".xlsx", "Yes");
+        }
+
+        /// <summary>
+        /// getting data from excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnExcel_Click(object sender, EventArgs e)
+        {
+            string excelfilepath = Server.MapPath("~/Upload/User.xlsx");
+            ImoprtExceltoGridView(excelfilepath, ".xlsx", "Yes");
+        }
+
+        /// <summary>
+        /// importing excel to gridview
+        /// </summary>
+        /// <param name="File"></param>
+        /// <param name="extension"></param>
+        /// <param name="ishdr"></param>
+        public void ImoprtExceltoGridView(string File, string extension, string ishdr)
+        {
+             string connectionStr = ConfigurationManager.ConnectionStrings["Excelconnection"].ConnectionString;
+            connectionStr = String.Format(connectionStr, File, ishdr);
+            OleDbConnection con = new OleDbConnection(connectionStr);
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+            DataTable dt = new DataTable();
+            cmd.Connection = con;
+            con.Open();
+            DataTable dtexcel = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string sheetname = dtexcel.Rows[0]["TABLE_NAME"].ToString();
+            cmd.CommandText = "Select * from [" + sheetname + "]";
+            oda.SelectCommand = cmd;
+            oda.Fill(dt);
+            con.Close();
+            gvFile.DataSource = dt;
+            gvFile.DataBind();
+        }
+
+        /// <summary>
+        /// getting data from csv file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnCSV_Click(object sender, EventArgs e)
+        {
+            string csvPath = Server.MapPath("~/Upload/User.csv");
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[4] { new DataColumn("Id"),
+            new DataColumn("Name"),
+            new DataColumn("Address"),
+            new DataColumn("Email")});
+            string csvData = File.ReadAllText(csvPath);
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+            gvFile.DataSource = dt;
+            gvFile.DataBind();
+        }
+       
+        /// <summary>
+        /// getting data from text file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnText_Click(object sender, EventArgs e)
+        {
+            string csvPath = Server.MapPath("~/Upload/User.txt");
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[4] { new DataColumn("Id"),
+            new DataColumn("Name"),
+            new DataColumn("Address"),
+            new DataColumn("Email")});
+            string csvData = File.ReadAllText(csvPath);
+            foreach (string row in csvData.Split('\n'))
+            {
+                if (!string.IsNullOrEmpty(row))
+                {
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (string cell in row.Split(','))
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+            }
+            gvFile.DataSource = dt;
+            gvFile.DataBind();
+        }
+    }
+}
