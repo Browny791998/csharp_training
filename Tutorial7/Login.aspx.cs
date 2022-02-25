b7269dac63da8fcc31bde5f755c3240f070ab25b
Browny791998 <69458990+Browny791998@users.noreply.github.com>
2022-02-25 16:48:53 +0630

    [feature/tutorial10]finish tutorial10 100%

--- Tutorial7/Login.aspx.cs
+++ Tutorial7/Login.aspx.cs
@@ -16,12 +16,11 @@ namespace Tutorial7
         {
 
         }
-
+        string constring = ConfigurationManager.ConnectionStrings["userDB"].ConnectionString;
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            string constring = ConfigurationManager.ConnectionStrings["userDB"].ConnectionString;
             SqlConnection con = new SqlConnection(constring);
-            SqlCommand cmd = new SqlCommand("SELECT * FROM tbl_User WHERE Name=@Name and Password=@Password", con);
+            SqlCommand cmd = new SqlCommand("SELECT Password FROM tbl_User WHERE Name=@Name and Password=@Password", con);
             cmd.Parameters.AddWithValue("Name", txtUserName.Text);
             cmd.Parameters.AddWithValue("Password", txtPassword.Text);
             SqlDataAdapter sda = new SqlDataAdapter(cmd);
@@ -34,13 +33,35 @@ namespace Tutorial7
             {
                 Session["name"] = txtUserName.Text;
                 Response.Redirect("Default.aspx");
-                Session.RemoveAll();
             }
             else
             {
-                LtlMessage.Text = "Username or Password is invalid";
+                SqlConnection con2 = new SqlConnection(constring);
+                string name = txtUserName.Text;
+                int id;
+                SqlCommand cmd2 = new SqlCommand("SELECT * FROM tbl_User WHERE Name='" + name + "'", con2);
+                con2.Open();
+                SqlDataReader dr = cmd2.ExecuteReader();
+                while (dr.Read())
+                {
+                    id = Convert.ToInt32(dr["id"]);
+                    name = dr["Name"].ToString();
+                    reset_password(id, name);
+                }
+                con2.Close();
             }
+        }
 
+        private void reset_password(int id, string name)
+        {
+            Session["Email"] = name;
+            SqlConnection con3 = new SqlConnection(constring);
+            SqlCommand cmd3 = new SqlCommand("Update tbl_User set Password=NULL,Status=1 WHERE id=@id", con3);
+            cmd3.Parameters.AddWithValue("id", id);
+            con3.Open();
+            cmd3.ExecuteNonQuery();
+            con3.Close();
+            Response.Redirect("Reset.aspx");
         }
     }
 }
