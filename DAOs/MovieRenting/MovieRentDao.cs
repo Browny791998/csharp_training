c5c262decee1afe23e4e1e9efb79ca0cd7dfedec
Browny791998 <69458990+Browny791998@users.noreply.github.com>
2022-03-08 17:13:37 +0630

    assignment 4 finish 100%

--- DAOs/MovieRenting/MovieRentDao.cs
+++ DAOs/MovieRenting/MovieRentDao.cs
@@ -101,7 +101,8 @@ namespace DAOs.MovieRenting
         {
             try
             {
-                return Common.HelperDao.GetData("Select tbl_movierenting.id,tbl_salutation.salutation,tbl_customer.full_name,tbl_customer.address,tbl_movie.movie from tbl_customer,tbl_movierenting,tbl_salutation,tbl_movie Where  tbl_salutation.id = tbl_customer.salutation_id AND tbl_movie.id = tbl_movierenting.movie_id AND tbl_customer.id = tbl_movierenting.customer_id", CommandType.Text);
+                //return Common.HelperDao.GetData("Select tbl_movierenting.id,tbl_salutation.salutation,tbl_customer.full_name,tbl_customer.address,tbl_movie.movie from tbl_customer,tbl_movierenting,tbl_salutation,tbl_movie Where  tbl_salutation.id = tbl_customer.salutation_id AND tbl_movie.id = tbl_movierenting.movie_id AND tbl_customer.id = tbl_movierenting.customer_id", CommandType.Text);
+                return Common.HelperDao.GetData("Select * from vGetAllMovieRentingData", CommandType.Text);
             }
             catch (Exception ex)
             {
@@ -119,12 +120,14 @@ namespace DAOs.MovieRenting
             {
                 if (str == "")
                 {
-                    return Common.HelperDao.GetData("Select tbl_movierenting.id,tbl_salutation.salutation,tbl_customer.full_name,tbl_customer.address,tbl_movie.movie from tbl_customer,tbl_movierenting,tbl_salutation,tbl_movie Where  tbl_salutation.id = tbl_customer.salutation_id AND tbl_movie.id = tbl_movierenting.movie_id AND tbl_customer.id = tbl_movierenting.customer_id", CommandType.Text);
+                    //return Common.HelperDao.GetData("Select tbl_movierenting.id,tbl_salutation.salutation,tbl_customer.full_name,tbl_customer.address,tbl_movie.movie from tbl_customer,tbl_movierenting,tbl_salutation,tbl_movie Where  tbl_salutation.id = tbl_customer.salutation_id AND tbl_movie.id = tbl_movierenting.movie_id AND tbl_customer.id = tbl_movierenting.customer_id", CommandType.Text);
+                    return Common.HelperDao.GetData("Select * from vGetAllMovieRentingData", CommandType.Text);
                 }
                 else
                 {
-                        return Common.HelperDao.GetData("Select tbl_movierenting.id,tbl_salutation.salutation,tbl_customer.full_name,tbl_customer.address,tbl_movie.movie from tbl_customer,tbl_movierenting,tbl_salutation,tbl_movie Where  tbl_salutation.id = tbl_customer.salutation_id AND tbl_movie.id = tbl_movierenting.movie_id AND tbl_customer.id = tbl_movierenting.customer_id AND tbl_customer.full_name ='" + str + "' ", CommandType.Text);
-                 }
+                //    return Common.HelperDao.GetData("Select tbl_movierenting.id,tbl_salutation.salutation,tbl_customer.full_name,tbl_customer.address,tbl_movie.movie from tbl_customer,tbl_movierenting,tbl_salutation,tbl_movie Where  tbl_salutation.id = tbl_customer.salutation_id AND tbl_movie.id = tbl_movierenting.movie_id AND tbl_customer.id = tbl_movierenting.customer_id AND tbl_customer.full_name ='" + str + "' ", CommandType.Text);
+                   return Common.HelperDao.GetData("Select * from vGetAllMovieRentingData Where full_name ='" + str + "' ", CommandType.Text);
+                }
             }
             catch (Exception ex)
             {
